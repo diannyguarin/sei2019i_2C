fd2f663e99d04bbfa140e62376da3284f083bb01
Diana Carolina Guarin <dcguarina@unal.edu.co>
2019-07-03 21:36:11 -0500

    Layouts added

---
+++ code/Tournament/app/src/main/java/com/example/tournaments/Presentation/ChDataActivity.java
@@ -0,0 +1,32 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChDataActivity extends AppCompatActivity {
+
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_changeData);
+
+
+
+        }
+/** Called when the user taps the Cancel button */
+public void goBack(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        }
+
+public void regComplete(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, LoginActivity.class);
+
+        startActivity(intent);
+        }
+        }
