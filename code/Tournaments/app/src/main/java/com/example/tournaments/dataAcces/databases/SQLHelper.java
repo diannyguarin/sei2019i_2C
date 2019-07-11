421b1ebe9bc46990ccfba9ea697b2abe5195e479
Diana Carolina Guarin <dcguarina@unal.edu.co>
2019-07-10 22:33:28 -0500

    pre-sprint2 version

--- code/Tournaments/app/src/main/java/com/example/tournaments/dataAcces/databases/SQLHelper.java
+++
@@ -1,8 +0,0 @@
-package com.example.tournaments.dataAcces.databases;
-
-public class SQLHelper {
-    public static final String url="jdbc:mysql://www.remotemysql.com:3306/pwEIUN4GpK?connectTimeout=3000&useSSL=false";
-    public static final String usr="pwEIUN4GpK";
-    public static final String pwd="mu0QblJ1yx";
-    public static final String driver="com.mysql.jdbc.Driver";
-}
